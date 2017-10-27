3cb033ee5bece414d93a37dd843b024adb1bb800
zuojianjun <zuojianjun@tuofeng.cn>
2017-10-27 14:45:27 +0800

    定义方法

--- index.ios.js
+++ index.ios.js
@@ -4,10 +4,16 @@ import { NativeModules } from 'react-native';
 
 const TFChangeAppIcon = NativeModules.TFChangeAppIcon;
 
-export default class ChangeAppIcon {
-    static changeAppIcon(info, callBack) {
-        TFChangeAppIcon.changeAppIconWithInfo(info, (respone) => {
-            callBack(respone);
-        });
-    }
+export default function changeAppIcon(info, callBack) {
+    TFChangeAppIcon.changeAppIconWithInfo(info, (respone) => {
+        callBack(respone);
+    });
 }
+
+// export default class ChangeAppIcon {
+//     static changeAppIcon(info, callBack) {
+        // TFChangeAppIcon.changeAppIconWithInfo(info, (respone) => {
+        //     callBack(respone);
+        // });
+//     }
+// }
